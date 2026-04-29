e8376d4c5e0ddd344b90473c5e3e798fdc3f56e0
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2026-04-29 00:09:16 -0500

    Quinto

--- src/pages/Quinto.jsx
+++ src/pages/Quinto.jsx
@@ -1,23 +1,31 @@
-/*
-
-  1- Props 
-
-*/
-
 import Sexto from "./Sexto"
-
+import imagenLubswer from "../../public/imgLubswer.jpeg"
+import imagenGabriel from "../../public/imgGabriel.jpeg"
+import imagenJossue from "../../public/imgJossue.jpeg"
 
 const Quinto = () => {
 
-  const userProfile = {
-    name: "Matew",
-    rol: "Employee",
-    email: "matew@gmail.com",
+  const integrante1 = {
+    name: "Lubswer Catagña",
+    rol: "Scrum Máster",
+    img: imagenLubswer,
+  }
+
+    const integrante2 = {
+    name: "Gabriel Toaquiza",
+    rol: "Frontend developer",
+    img: imagenGabriel,
+  }
+
+    const integrante3 = {
+    name: "Jossue Chulde",
+    rol: "Backend developer",
+    img: imagenJossue,
   }
 
   return (
     <>
-      <h1 className="font-bold text-2xl">Props</h1>
+      <h1 className="font-bold text-2xl">Props - Ejemplo</h1>
 
       <hr className="border-gray-400 mb-8"/>
 
@@ -27,11 +35,13 @@ const Quinto = () => {
         </li>
       </ul>
       
-      <h2 className="text-green-700 text-center mt-8">Padre</h2>
+      <h2 className="text-green-700 text-center mt-8">Estudiantes (Padre)</h2>
       
       <div className="border-2 border-green-500 flex-colum justify-center items-center w-120 mx-auto mb-8">
     
-        <Sexto data={userProfile}/>
+        <Sexto data={integrante1}/>
+        <Sexto data={integrante2}/>
+        <Sexto data={integrante3}/>
         
       </div>
     
