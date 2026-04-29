a4a2d95b7580a7fd3be4a01e055802f75f93ae93
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2026-04-29 00:14:14 -0500

    Noveno

--- src/pages/Noveno.jsx
+++ src/pages/Noveno.jsx
@@ -1,85 +1,108 @@
-/*
-
-  1- Rederizado condicional
-  2- Renderizado de listas
-
-*/
-
-import { useState } from "react"
-
-
+import { useState } from "react";
 
 const Noveno = () => {
+  const [login, setLogin] = useState(false);
 
-  const [login, setLogin] = useState(false)
-  const [habilidades] = useState([
-    { nombre: "HTML", icono: "📙" },
-    { nombre: "CSS", icono: "📘" },
-    { nombre: "JavaScript", icono: "📒" },
-    { nombre: "React", icono: "⚛️" },
-    { nombre: "Node.js", icono: "📗" },
-  ])
+  const [materiasTercerSemestre] = useState([
+    { materia: "PROGRAMACIÓN ORIENTADA A OBJETOS", creditos: 4 },
+    { materia: "DISEÑO DE INTERFACES", creditos: 2 },
+    { materia: "GESTIÓN DE PROYECTOS DE SOFTWARE", creditos: 3 },
+    { materia: "BASES DE DATOS", creditos: 3 },
+    { materia: "ANÁLISIS DE DATOS", creditos: 3 },
+  ]);
+
+  const [materiasCuartoSemestre] = useState([
+    { materia: "DESARROLLO DE APLICACIONES WEB", creditos: 4 },
+    { materia: "DESARROLLO DE loT", creditos: 2 },
+    { materia: "FUNDAMENTOS DE INTELIGENCIA ARTIFICIAL", creditos: 1 },
+    { materia: "METODOLOGÍA DE LA INVESTIGACIÓN", creditos: 1 },
+    { materia: "PRÁCTICAS DE SERVICIO COMUNITARIO", creditos: 2 },
+    { materia: "PRÁCTICAS LABORALES", creditos: 5 },
+  ]);
 
   return (
     <>
-      <h1 className="font-bold text-2xl">Renderizado</h1>
+      <h1 className="font-bold text-2xl">Renderizado - Ejemplo</h1>
 
-      <hr className="border-gray-400 mb-8"/>
+      <hr className="border-gray-400 mb-8" />
 
       <ul className="list-disc pl-5">
         <li>
-        <strong>Renderizado condicional:</strong> Permite mostrar u ocultar elementos de la interfaz según ciertas condiciones.
+          <strong>Renderizado condicional:</strong> Permite mostrar u ocultar
+          elementos de la interfaz según ciertas condiciones.
         </li>
         <li>
-          <strong>Renderizado de listas:</strong> Permite mostrar múltiples elementos a partir de un array usando métodos de los arreglos.
+          <strong>Renderizado de listas:</strong> Permite mostrar múltiples
+          elementos a partir de un array usando métodos de los arreglos.
         </li>
       </ul>
 
       <div className="flex justify-center mb-8 mt-8">
         <div className="border rounded-lg p-4 w-120 mx-auto text-center">
-          
-          <h2 className="text-1xl font-bold text-left underline mb-4">Condicional</h2>
+          <h2 className="text-1xl font-bold text-left underline mb-4">
+            Condicional
+          </h2>
 
-          {login 
-            ? 
-          (
-            <p>¡Bienvenido de nuevo! 😎 </p>
-          ) 
-            : 
-          (
-           <p>Por favor, inicia sesión</p>
+          {login ? (
+            <p>
+              Ya estas en el Cuarto Semestre, preparate para las próximas
+              materias
+            </p>
+          ) : (
+            <p>Revisa la lista de materias del tercer semestre.</p>
           )}
 
-          <button className="mt-2 bg-blue-600 text-white py-1 px-3 rounded" onClick={() => setLogin(!login)}>
-            {login ? "Cerrar sesión" : "Iniciar sesión"}
+          <button
+            className={`mt-2 text-white py-1 px-3 rounded ${
+              login ? "bg-orange-600" : "bg-green-600"
+            }`}
+            onClick={() => setLogin(!login)}
+          >
+            {login ? "No pase todas las materias" : "3er Semestre Completado "}
           </button>
-
         </div>
-
       </div>
 
-      <div className="flex justify-center mb-8">
+      {login ? (
+        <div className="flex justify-center mb-8">
+          <div className=" border rounded-lg p-4 w-120 mx-auto text-center">
+            <h2 className="text-1xl font-bold text-left underline mb-4">
+              Listas
+            </h2>
+            <h2 className="text-red-700 font-bold text-left mb-4">
+              Creditos - Materias
+            </h2>
 
-        <div className=" border rounded-lg p-4 w-120 mx-auto text-center">
-
-          <h2 className="text-1xl font-bold text-left underline mb-4">Listas</h2>
-          
             <ul className="list-disc pl-5 text-left">
-              
-              {habilidades.map((habilidad, index) => (
-      
-                <li key={index}>{habilidad.icono} - {habilidad.nombre}</li>
-      
+              {materiasCuartoSemestre.map((materia, index) => (
+                <li key={index}>
+                  {materia.creditos} - {materia.materia}
+                </li>
               ))}
-            
             </ul>
-          
+          </div>
         </div>
-
-      </div>
-
+      ) : (
+        <div className="flex justify-center mb-8">
+          <div className=" border rounded-lg p-4 w-120 mx-auto text-center">
+            <h2 className="text-1xl font-bold text-left underline mb-4">
+              Listas
+            </h2>
+            <h2 className="text-green-700 font-bold text-left mb-4">
+              Creditos - Materias
+            </h2>
+            <ul className="list-disc pl-5 text-left">
+              {materiasTercerSemestre.map((materia, index) => (
+                <li key={index}>
+                  {materia.creditos} - {materia.materia}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </>
-  )
-  }
+  );
+};
 
-export default Noveno
+export default Noveno;
