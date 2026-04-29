b1873843285ad33838ade2440f453823cfa9f7af
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2026-04-29 00:11:22 -0500

    Octavo

--- src/pages/Octavo.jsx
+++ src/pages/Octavo.jsx
@@ -1,12 +1,4 @@
-/*
-
-  1- Zustand 
-
-*/
-
-import storeGalleta from "../context/storeGalleta"
-
-
+import storeGalleta from "../context/storeTecnologia"
 
 const Octavo = () => {
 
@@ -15,15 +7,17 @@ const Octavo = () => {
   return (
     <>
 
-      <h2 className="text-purple-700">Hijo</h2>
+      <h2 className="text-cyan-700">Hijo</h2>
 
       <div className="flex justify-center mb-8">
 
-        <div className="max-w-sm border-2 border-purple-500 rounded-lg p-4 w-80 text-center">
+        <div className="max-w-sm border-2 border-cyan-500 rounded-lg p-4 w-80 text-center">
 
-          <p>nombre: {detalle.nombre}</p>
+          <p>Nombre: {detalle.nombre}</p>
 
-          <p>tipo: {detalle.tipo}</p>
+          <p>Tipo: {detalle.tipo}</p>
+
+          <p>Precio: ${detalle.precio.toFixed(2)}</p>
 
         </div>
 
