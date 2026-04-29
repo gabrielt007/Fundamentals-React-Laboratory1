c60a098ce360b58ddbb7962e51e75035b74aecad
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2026-04-29 00:10:00 -0500

    Sexto

--- src/pages/Sexto.jsx
+++ src/pages/Sexto.jsx
@@ -1,27 +1,20 @@
-/*
-
-  1- Props 
-
-*/
-
-
 const Sexto = ({data}) => {
 
 
   return (
     <>
 
-      <h2 className="text-orange-800">Hijo</h2>
+      <h2 className="text-orange-800">Integrante (Hijo)</h2>
       
       <div className="flex justify-center mb-8">
         
         <div className="max-w-sm border-2 border-orange-500 rounded-lg p-4 w-80 text-center">
           
-          <h2 className="text-lg font-semibold mb-2">Bienvenido(a) - {data.name}</h2>
+          <h2 className="text-lg font-semibold mb-2">Bienvenido - {data.name}</h2>
 
-          <p className="mb-3">rol: {data.rol}</p>
+          <p className="mb-3">Rol: {data.rol}</p>
           
-          <p className="mb-3">email: {data.email}</p>
+          <img src={data.img} alt={data.name} className="mb-3"/>
         
         </div>
 
